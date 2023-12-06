4f2d98b72a53908d844567389a42bc97c97f6461
joyel <joyelroy24@gmail.com>
2023-12-06 11:55:22 +0530

    variables and render data data display using for included

--- proname/newapp/views.py
+++ proname/newapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 
 def printhello(request):
-    return HttpResponse("Helloii")
+    data={}
+    data['key']="hello Guyss"
+    data['listkey']=[{'key1':'hello'},{'key1':'heyy'}]
+    return render(request,'test.html',data) 
