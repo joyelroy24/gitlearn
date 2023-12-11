4596bcf2b5bb0ce1475b965591479c640a72643d
joyel <joyelroy24@gmail.com>
2023-12-11 21:17:17 +0530

    one to one relation established

--- movie_manager/movies/models.py
+++ movie_manager/movies/models.py
@@ -7,5 +7,13 @@ class Movieinfo(models.Model):
 
 class Director(models.Model):
     name=models.CharField(max_length=100)
+    certified_by=models.CharField(max_length=200,null=True)
+
+
+class Censorinfo(models.Model):
+    rating=models.CharField(max_length=10,null=True)
+    movie_detail=models.OneToOneField(Movieinfo,on_delete=models.SET_NULL,related_name='censor',null=True)
+    
+
 
 
